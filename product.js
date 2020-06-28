8059fed1166c44536af5bf75c1d511bb1bdc4e56
Trinacapri <46378058+Trinacapri@users.noreply.github.com>
2020-06-28 21:05:27 +0530

    Add files via upload

--- product.js
+++ product.js
@@ -2,11 +2,23 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const productSchema = new Schema({
   //create a new schema for product
-  categoryid: String,
   productname: String,
-  productprice: Number,
-  isActive: Boolean,
+  productprice: String,
+  productdiscount: String,
+  productpriceWithDiscount: String,
+  productdoseInMG: String,
+  productmgfdate: String,
+  productexpiredate: String,
+  productdescription: String,
+  productimage:String, 
 
+  categoryid: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "categories",
+    required: true,
+  },
+
+  // isActive: Boolean,
 });
 
 const products = mongoose.model("products", productSchema); //collection name is products
